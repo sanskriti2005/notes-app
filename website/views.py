45c770a240eb0a47039b573f71189606e5ed262a
Sanskriti <sanskritisri27@gmail.com>
2024-04-19 07:29:48 +0000

    worked on the navbar

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 #create/set-up a blueprint
 views = Blueprint('views', __name__)
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 #create a route for that blueprint
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
